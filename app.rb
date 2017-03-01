d9e1fadd4d084075cbd89a2bd28b0c499662854a
John Chang <johnchang9094@gmail.com>
2017-03-01 16:08:07 +0000

    Redirect

--- app.rb
+++ app.rb
@@ -2,18 +2,22 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  get '/' do
-    'Testing infrastructure working!'
-  end
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    #p params
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    p params
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb :play
   end
 
--- spec/feature/testing_infrastructure_spec.rb
+++ spec/feature/testing_infrastructure_spec.rb
@@ -1,17 +1,12 @@
 
-feature 'Homepage displays working infrastructure message' do
-  scenario 'checks for content' do
-    visit '/'
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end
+
 
 feature 'Expects players to fill in names in a form' do
   scenario 'To fill in names' do
-    visit ('/')
+    visit '/'
     fill_in(:player_1, with: 'John')
     fill_in(:player_2, with: 'Ryan')
     click_button('Submit')
-    expage(page).to have_content 'Ryan, John'
+    expect(page).to have_content 'The names are John, Ryan'
   end
 end
