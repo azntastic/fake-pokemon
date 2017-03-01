b33727976b7abf72f14545517d46614ce274ad02
John Chang <johnchang9094@gmail.com>
2017-03-01 16:48:31 +0000

    Test Helpers

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,20 +1,21 @@
 
+feature 'Enter names' do
+  scenario 'Entering names' do
+    sign_in_and_play
+    expect(page).to have_content 'The names are John, Ryan'
+  end
+end
+
 feature 'Expects players to fill in names in a form' do
   scenario 'To fill in names' do
-    visit '/'
-    fill_in(:player_1, with: 'John')
-    fill_in(:player_2, with: 'Ryan')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'The names are John, Ryan'
   end
 end
 
 feature 'displays numbers' do
   scenario 'players have hitpoints' do
-    visit '/'
-    fill_in(:player_1, with: 'John')
-    fill_in(:player_2, with: 'Ryan')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content "John has 20 hp"
   end
 end
